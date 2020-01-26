6e95d6992f987ef1e4933a6b5e9d837997d4fec9
TonyWang <448574653@qq.com>
2020-01-26 15:12:22 +0800

    paint line on canvas

--- assets/canvas.js
+++ assets/canvas.js
@@ -1,13 +1,30 @@
 window.onload = () => {
+  let state = { painting: false };
+  const setState = (newState) => { return state = newState };
   const canvas = document.getElementById("canvas");
+  const ctx = canvas.getContext("2d");
 
-  canvas.onclick = (e) => {
-    const point = document.createElement('div');
+  canvas.width = document.documentElement.clientWidth;
+  canvas.height = document.documentElement.clientHeight;
 
-    point.className = "point";
-    point.style.left = e.clientX + 'px';
-    point.style.top = e.clientY + 'px';
+  canvas.onmousedown = () => {
+    return setState({
+      painting: true
+    });
+  };
 
-    return canvas.appendChild(point);
+  canvas.onmouseup = () => {
+    return setState({
+      painting: false
+    });
+  };
+
+  canvas.onmousemove = (e) => {
+    if(state.painting) {
+      ctx.fillStyle = "red"
+      ctx.beginPath();
+      ctx.arc(e.clientX, e.clientY, 6, 0, 2 * Math.PI, false);
+      ctx.fill();
+    }
   };
 };
